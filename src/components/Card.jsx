bcf72e68eb7e4f7239789bf1d8d123bbcc7b0767
bahadurmirza1 <mirzabahadurlatif@gmail.com>
2024-03-23 14:27:35 +0500

    change in image alt

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card({product}) {
     <>
     <div className='col-md-3'>
     <div className="card">
-  <img src= {product.img} className="card-img-top" alt="..."/>
+  <img src= {product.img} className="card-img-top" alt=""/>
   <div className="card-body">
     <h5 className="card-title">{product.title}</h5>
     <p className="card-text">{product.describ}</p>
